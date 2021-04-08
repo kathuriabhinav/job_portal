496f986d6a79ec38a0dcbcc6e3b9f0e210e81094
Kathuri Abhinav <kathuriabhinav@gmail.com>
2021-04-08 13:18:17 +0530

    Add files via upload

---
+++ job_portal_system/src/job_portal_system/my_main_class.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job_portal_system;
+
+/**
+ *
+ * @author ABHINAV KATHURI
+ */
+public class my_main_class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        main_screen m = new main_screen();
+        m.setLocationRelativeTo(null);
+        m.setDefaultCloseOperation(3);
+        m.setVisible(true);
+    }
+    
+}
